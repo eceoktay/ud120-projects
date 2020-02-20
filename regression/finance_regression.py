1c834ff8c06f7bf526f4205b597999637c8bd676
eceoktay <ece__oktay@hotmail.com>
2020-02-20 20:43:54 +0300

    Updated regression exercise

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -16,20 +16,23 @@
 import sys
 import pickle
 sys.path.append("../tools/")
+from time import time
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+# Modification#1: Regressing Bonus Against LTI
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,12 +40,29 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+
+t0 = time()
+reg.fit(feature_train, target_train)
+print "training time:", round(time()-t0, 3), "s"
+
+print "Slope: "
+print reg.coef_
+print "Intercept: "
+print reg.intercept_
+
+# r-squared score on train dataset
+stats_train = reg.score(feature_train, target_train)
+print "R-square on train: "
+print stats_train
 
 
 
-
-
-
+# r-squared score on test dataset
+stats_train = reg.score(feature_test, target_test)
+print "R-square on test: "
+print stats_train
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +84,23 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+# Modification#2: Changing test and train sets
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print "Slope: "
+print reg.coef_
+print "Intercept: "
+print reg.intercept_
+# r-squared score on train dataset
+stats_train = reg.score(feature_test, target_test)
+print "R-square on train: "
+print stats_train
+# r-squared score on test dataset
+stats_train = reg.score(feature_train, target_train)
+print "R-square on test: "
+print stats_train
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
