5c6b5a4236911680da56a524039bcfb4e7cb6c63
eceoktay <ece__oktay@hotmail.com>
2020-02-24 19:48:00 +0300

    Updated text learning stemming exercise

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -2,6 +2,7 @@
 
 from nltk.stem.snowball import SnowballStemmer
 import string
+import re # Regular expression operations
 
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
@@ -28,11 +29,17 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
+        word_list = re.sub("[^\w]", " ",  text_string).split()
+        stemmer = SnowballStemmer("english")
+        for word in word_list:
+            if (words != ""):
+                words += " "
+            words += stemmer.stem(word)
         
 
 
