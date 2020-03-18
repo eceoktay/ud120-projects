275d8c36956cc60c1060372b0b4fcdb4cea2697e
eceoktay <ece__oktay@hotmail.com>
2020-03-18 12:19:23 +0300

    Updated validation exercise

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,5 +28,18 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn import tree
+clf = tree.DecisionTreeClassifier() 
+#clf.fit(features, labels)
+#acc = clf.score(features, labels)
+#print "Accuracy: "
+#print acc
+
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+clf.fit(features_train, labels_train)
+acc = clf.score(features_test, labels_test)
+print "Accuracy: "
+print acc
 
 
